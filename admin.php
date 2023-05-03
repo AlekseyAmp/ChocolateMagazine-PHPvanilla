9eaf9b57f0c35c6bc59f4f50aad8e71eac0d8a50
AlekseyAmp <thientaovv@gmail.com>
2023-05-03 13:53:47 +0500

    Wrapped the echo errors in the p tag

--- admin.php
+++ admin.php
@@ -32,7 +32,7 @@ elseif (isset($_POST['delete_product'])) {
         header('location: admin');
         exit();
     } else {
-        echo "Ошибка";
+        echo "<p class='white-text'>Ошибка</p>";
     }
 }
 ?>
@@ -45,7 +45,7 @@ elseif (isset($_POST['delete_product'])) {
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../assets/css/styles.css">
-    <title>Добавить товар</title>
+    <title>Админ панель</title>
 </head>
 
 <body>
